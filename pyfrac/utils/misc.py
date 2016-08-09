2d45a2002d33c22ffbb050863dfc4b74ed0d174e
mohitsharma44 <mohitsharma44@gmail.com>
2016-08-09 22:09:01 +0000

    added function to return patch files

--- pyfrac/utils/misc.py
+++ pyfrac/utils/misc.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 from pyfrac.utils import pyfraclogger
+import os
 
 logger = pyfraclogger.pyfraclogger(tofile=True)
 
@@ -10,3 +11,23 @@ def ignored(*exception):
         yield
     except exception, e:
         logger.warning(str(e))
+
+def getPatchLoc(pan, tilt):
+    """
+    Get pixel patches for file name
+    corresponding with (p|n)pan_(p|n)tilt.conf
+    
+    Parameters
+    ----------
+    pan : str
+    tilt : str
+
+    Returns
+    -------
+    fileloc : str
+        Corresponding patch file location.
+
+    .. note:: Refer `controlloop.py` and `tojpg()` function 
+              in `csvtojpg` module
+    """
+    return os.path.abspath('patches/%s_%s.conf'%(pan, tilt))
