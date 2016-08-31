7a9701afadf2d49c412263b6508978c686925f9c
mohitsharma44 <mohitsharma44@gmail.com>
2016-08-31 14:12:30 -0400

    minor mods

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 sleep 10
 date >> /home/pi/devel/pyfrac_logs/pyfrac_crash_times.txt
+# Kill omxplayer and the script if it has not been closed properly
 kill -9 $(pidof omxplayer*)
+kill -9 $(pidof control*)
 python /home/pi/devel/pyfrac/controlloop.py
-omxplayer -o hdmi --live rtsp://192.168.1.4 --win 0,0,1366,768
+omxplayer -o hdmi --live rtsp://192.168.1.4 --win 0,0,320,240
