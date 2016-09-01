8d17dc2b5102e410317656582c7e63f2d2fdaeac
mohitsharma44 <mohitsharma44@gmail.com>
2016-09-01 09:43:44 -0400

    created an iterator cycle for all positions

--- controlloop.py
+++ controlloop.py
@@ -6,6 +6,7 @@ from pyfrac.serve import serve
 from pyfrac.utils import misc
 import os
 import time
+from itertools import cycle
 
 CONFIG_FILE = "movement.conf"
 IR_IMAGE_DIR = "/home/pi/Pictures/pyfrac_images"
@@ -31,72 +32,71 @@ def initialize():
 
 def runTask(positions):
     try:
+        # Create an iterator cycle for the positions
+        position_cycle = cycle(positions)
+        position = position_cycle.next()
         logger.info("Performing NUC")
         cam.nuc()
         #cam.focus("full")
         #while not cam.ready():
         #    logger.debug("Performing Full Focus")
         #    time.sleep(1)
-        for position in positions:
-            try:
-                logger.info("Moving to position: " + str(position))
-                pan_pos = position.split('_')[0]
-                tilt_pos = position.split('_')[1].split(',')[0]
-                patchfile = misc.getPatchLoc(pan_pos,tilt_pos)
-
-                # ---- Hacky way to remove p and n from pan and tilt
-                # and replace them with + / - signs
-                if "p" in pan_pos:
-                    pan_pos = pan_pos.strip("p")
-                elif "n" in pan_pos:
-                    pan_pos = "-"+pan_pos.strip("n")
-                if "p" in tilt_pos:
-                    tilt_pos = tilt_pos.strip("p")
-                elif "n" in pan_pos:
-                    tilt_pos = "-"+tilt_pos.strip("n")
-                # ----
+        logger.info("Moving to position: " + str(position))
+        pan_pos = position.split('_')[0]
+        tilt_pos = position.split('_')[1].split(',')[0]
+        patchfile = misc.getPatchLoc(pan_pos,tilt_pos)
+        
+        # ---- Hacky way to remove p and n from pan and tilt
+        # and replace them with + / - signs
+        if "p" in pan_pos:
+            pan_pos = pan_pos.strip("p")
+        elif "n" in pan_pos:
+            pan_pos = "-"+pan_pos.strip("n")
+        if "p" in tilt_pos:
+            tilt_pos = tilt_pos.strip("p")
+        elif "n" in pan_pos:
+            tilt_pos = "-"+tilt_pos.strip("n")
+        # ----
+        
+        zoom_fac = position.split(',')[1]
+        keycontrol.pan(pan_pos)
+        keycontrol.tilt(tilt_pos)
+        
+        while not keycontrol.ready():
+            logger.debug("Waiting for PT module ")
+            time.sleep(1)
                 
-                zoom_fac = position.split(',')[1]
-                keycontrol.pan(pan_pos)
-                keycontrol.tilt(tilt_pos)
+        #cam.zoom(int(zoom_fac))
+        cam.focus("full")
+        
+        while not cam.ready():
+            logger.debug("Waiting for camera ")
+            time.sleep(1)
+
+        fname = cam.capture()
+        fname = fname+".jpg"
+        cam.fetch(filename="", pattern="jpg")
+        
+        # Conversion
+        meta_fname = converter.get_meta(tofile=True, filename=os.path.join(IR_IMAGE_DIR, fname))
+        time.sleep(1)
+        gray_fname = converter.tograyscale(meta=False, filename=os.path.join(IR_IMAGE_DIR, fname))
+        time.sleep(1)
+        logger.info(str(meta_fname))
+        logger.info(str(gray_fname))
+            
+        csv_fname = converter.tocsv(meta_fname, gray_fname)
+        if csv_fname:
+            png_fname = csvtojpg.tojpg(csv_fname, patchfile)
                 
-                while not keycontrol.ready():
-                    logger.debug("Waiting for PT module ")
-                    time.sleep(1)
+            # Upload
+            serve.uploadFile(os.path.abspath(png_fname))
+        else:
+            logger.error("no csv to png performed")
 
-                #cam.zoom(int(zoom_fac))
-                cam.focus("full")
-                    
-                while not cam.ready():
-                    logger.debug("Waiting for camera ")
-                    time.sleep(1)
-
-                fname = cam.capture()
-                fname = fname+".jpg"
-                cam.fetch(filename="", pattern="jpg")
-
-                # Conversion
-                meta_fname = converter.get_meta(tofile=True, filename=os.path.join(IR_IMAGE_DIR, fname))
-                time.sleep(1)
-                gray_fname = converter.tograyscale(meta=False, filename=os.path.join(IR_IMAGE_DIR, fname))
-                time.sleep(1)
-                logger.info(str(meta_fname))
-                logger.info(str(gray_fname))
-
-                csv_fname = converter.tocsv(meta_fname, gray_fname)
-                if csv_fname:
-                    png_fname = csvtojpg.tojpg(csv_fname, patchfile)
-
-                    # Upload
-                    serve.uploadFile(os.path.abspath(png_fname))
-                else:
-                    logger.error("no csv to png performed")
-            except Exception as ex:
-                logger.warning(str(ex))
     except Exception as ex:
-        logger.error(str(ex))        
-    finally:
-        runTask(positions)
+        logger.error(str(ex))
+
 
 if __name__ == "__main__":
     positions = initialize()
